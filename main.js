ef9d41bbb968478da9901ecb14d3716b30e81f16
sdlima4025 <sergiolimafg3@gmail.com>
2024-01-17 19:35:09 -0300

    criado js do count down. future de seleção de data na tela.

---
+++ main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const formatarDigito = (digito) => `0${digito}`.slice(-2);
+const atualizar = (tempo) => {
+    const segundos = document.getElementById('segundos');
+    const minutos = document.getElementById('minutos');
+    const horas = document.getElementById('horas');
+    const dias = document.getElementById('dias');
+
+    const qtdSegundos = tempo % 60;
+    const qtdMinutos = Math.floor((tempo % (60 * 60)) / 60);
+    const qtdHoras = Math.floor((tempo % (60 * 60 * 24)) / (60 * 60));
+    const qtdDias = Math.floor(tempo / (60 * 60 * 24));
+
+    segundos.textContent = formatarDigito(qtdSegundos);
+    minutos.textContent = formatarDigito(qtdMinutos);
+    horas.textContent = formatarDigito(qtdHoras);
+    dias.textContent = formatarDigito(qtdDias);
+}
+
+const contagemRegressica = (tempo) => {
+  const pararContagem = () => clearInterval(id);
+  const contar = () => {
+    if (tempo === 0) {
+      pararContagem();
+    }
+    atualizar (tempo);
+    tempo--;
+  };
+  const id = setInterval(contar, 1000);
+};
+
+const tempoRestante = () => {
+    // 1 de janeiro de 1970 inicio do uso de milisegundos
+    const dataEvento = new Date ('2024-01-17 20:00:00');
+    const hoje = Date.now();
+    return Math.floor((dataEvento - hoje) / 1000);
+}
+
+contagemRegressica(tempoRestante());
